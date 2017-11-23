5bfb8d32abb30ad3b1947656c8f5de4dcd25716c
ardit <ardit.halimi95@gmail.com>
2017-11-23 14:36:01 +0100

    pastrimi i kodit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    document.getElementById("grupi").innerHTML = "Grupi i dyte";
-}
-
-
-function emrimbiemri(name,mbiemri){
-    console.log("emri im eshte ",name,"dhe mbiemri im eshte ",mbiemri);
-}
-emrimbiemri("Ardit ","Halimi");
