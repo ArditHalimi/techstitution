40d1a45e0e7f9a6d1ab5b4c5664eda2a9aa12938
ardit <ardit.halimi95@gmail.com>
2017-11-24 14:33:12 +0100

    validimi,to json

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+
     console.log("Dom is ready!")
+
+
+
 
     $('#border-wait-form').submit(function (event) {
         event.preventDefault();
+        var date=new Date();
+
         var piaKufitare= $('#pika-kufitar').val();
         var hyrjeMinMinuta=$('#hyrje-min-minuta').val();
         var hyrjeMaxMinuta=$('#hyrje-max-minuta').val();
@@ -13,8 +19,21 @@ $(document).ready(function () {
         var hyrjeMaxMetra=$('#hyrje-max-metra').val();
         var daljeMinMetra=$('#dalje-min-metra').val();
         var daljeMaxMetra=$('#dalje-max-metra').val();
-        console.log("Pika Kufitare",piaKufitare,"Hyrje max minuta",hyrjeMinMinuta,"Hyrje max minuta",hyrjeMaxMinuta,"Dalje min minuta",daljeMinMinuta,"Dalje max minuta",daljeMaxMinuta,
-        "Hyrje min metra ",hyrjeMinMetra,"Hyrje max metra",hyrjeMaxMetra,"Dalja min metra",daljeMinMetra,"Dalje max metra",daljeMaxMetra);
+       /* console.log("Pika Kufitare",piaKufitare,"Hyrje max minuta",hyrjeMinMinuta,"Hyrje max minuta",hyrjeMaxMinuta,"Dalje min minuta",daljeMinMinuta,"Dalje max minuta",daljeMaxMinuta,
+        "Hyrje min metra ",hyrjeMinMetra,"Hyrje max metra",hyrjeMaxMetra,"Dalja min metra",daljeMinMetra,"Dalje max metra",daljeMaxMetra);*/
+
+        var dataJson={};
+        dataJson ["pikaKufitare"]=piaKufitare;
+        dataJson ["hyrjeMinMinuta"]=parseInt(hyrjeMinMinuta);
+        dataJson ["hyrjeMaxMinuta"]=parseInt(hyrjeMaxMinuta);
+        dataJson ["daljeMinMinuta"]=parseInt(daljeMinMinuta);
+        dataJson ["daljeMaxMinuta"]=parseInt(daljeMaxMinuta);
+        dataJson ["hyrjeMaxMetra"]=parseInt(hyrjeMaxMetra);
+        dataJson ["hyrjeMinMetra"]=parseInt(hyrjeMinMetra);
+        dataJson ["daljeMinMetra"]=parseInt(daljeMinMetra);
+        dataJson ["daljeMinMetra"]=parseInt(daljeMinMetra);
+        dataJson ["date"]=date;
+        console.log(dataJson);
 
     });
 });
